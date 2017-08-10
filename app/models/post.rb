4bae5932780d41406feefae6940bbbdc9063eef5
Joe <jdg006@lvc.edu>
2017-08-10 21:19:10 +0000

    assignment 27 complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,10 @@ class Post < ActiveRecord::Base
     has_many :comments, dependent: :destroy
     
     default_scope { order('created_at DESC') }
-    
+    scope :ordered_by_reverse_created_at, -> { order('created_at ASC') }
+    scope :ordered_by_title, -> {order('title')}
+   
+  
    validates :title, length: { minimum: 5 }, presence: true
    validates :body, length: { minimum: 20 }, presence: true
    validates :topic, presence: true
